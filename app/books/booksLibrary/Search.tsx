627442684f17c94c3cd5163d4b3badd25e71a866
kruci <rkuci24@gmail.com>
2025-04-10 00:59:55 +0200

    Add debounce to search

--- app/books/booksLibrary/Search.tsx
+++ app/books/booksLibrary/Search.tsx
@@ -1,33 +1,43 @@
 import { SearchOutlined } from "@ant-design/icons";
 import { Input, Tooltip } from "antd";
-import { ChangeEventHandler } from "react";
 import { useDispatch } from "react-redux";
 import { StoreDispatch } from "../store/appStore";
 import { updateSearchValue } from "../store/bookSearchSlice";
+import { debounce } from "lodash";
+import { useCallback, useMemo } from "react";
 
 export const Search = () => {
   const dispatch = useDispatch<StoreDispatch>();
 
-  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
-    const newValue = event.target.value;
-    dispatch(updateSearchValue(newValue));
-  };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const debouncedHandleChange = useCallback(
+    debounce((event: React.ChangeEvent<HTMLInputElement>) => {
+      const newValue = event.target.value;
+      dispatch(updateSearchValue(newValue));
+    }, 400),
+    [dispatch]
+  );
 
-  const handleClear = () => {
+  const handleClear = useCallback(() => {
     dispatch(updateSearchValue(""));
-  };
+  }, [dispatch]);
+
+  const renderedInputSuffix = useMemo(
+    () => (
+      <Tooltip title="Extra information">
+        <SearchOutlined style={{ color: "rgba(0,0,0,.45)" }} />
+      </Tooltip>
+    ),
+    []
+  );
 
   return (
     <Input
       placeholder="Search for books"
       allowClear
       onClear={handleClear}
-      onChange={handleChange}
-      suffix={
-        <Tooltip title="Extra information">
-          <SearchOutlined style={{ color: "rgba(0,0,0,.45)" }} />
-        </Tooltip>
-      }
+      onChange={debouncedHandleChange}
+      suffix={renderedInputSuffix}
     />
   );
 };
