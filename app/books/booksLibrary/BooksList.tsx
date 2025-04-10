e9a89fc3fce90f9fefa6360076a53d770059d4a1
kruci <rkuci24@gmail.com>
2025-04-10 03:18:02 +0200

    Add fuzzy search

--- app/books/booksLibrary/BooksList.tsx
+++ app/books/booksLibrary/BooksList.tsx
@@ -3,14 +3,29 @@ import { RootState } from "../store/appStore";
 import { BookListItem } from "./BookListItem";
 import { List } from "antd";
 import styles from "./BookList.module.css";
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { Book } from "../store/booksSlice";
 import { BookModal } from "./BookModal";
 import VirtualList from "rc-virtual-list";
+import Fuse from "fuse.js";
 
 export const BooksList = () => {
   const [selectedBook, setSelectedBook] = useState<Book>();
   const books = useSelector((state: RootState) => state.books.books);
+  const searchValue = useSelector(
+    (state: RootState) => state.bookSearch.searchValue
+  );
+
+  const fuseInstance = useMemo(() => {
+    return new Fuse(books, { keys: ["title"] , threshold: 0.3});
+  }, [books]);
+
+  const filteredBooks = useMemo(() => {
+    if (!searchValue) {
+      return books;
+    }
+    return fuseInstance.search(searchValue).map((result) => result.item);
+  }, [books, fuseInstance, searchValue]);
 
   const handleBookClick = useCallback((book: Book) => {
     setSelectedBook(book);
@@ -25,7 +40,7 @@ export const BooksList = () => {
     <>
       <List className={styles.list}>
         <VirtualList
-          data={books}
+          data={filteredBooks}
           height={450}
           itemHeight={73}
           itemKey="title"
